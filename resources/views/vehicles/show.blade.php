4b9e7fc913c56c4949c69fcc83b2eff46fe82b7d
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-07-29 00:06:33 +0500

    Update show.blade.php

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -65,7 +65,7 @@
                                         @endif
                                     </table>
                                     @if($vehicle->winning_screenshot_path)
-                                        <a href="{{ \Storage\app\public::url($vehicle->winning_screenshot_path) }}" target="_blank" class="btn btn-sm btn-outline-secondary">
+                                        <a href="{{ \Storage::url($vehicle->winning_screenshot_path) }}" target="_blank" class="btn btn-sm btn-outline-secondary">
                                             <i class="fa fa-image"></i> View Winning Screenshot
                                         </a>
                                     @endif
