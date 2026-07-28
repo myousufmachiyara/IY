68e6ba1eb3d74bf0a0c2cfe97dbb8500cd54268d
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-07-28 22:43:39 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -131,7 +131,6 @@
                     <a href="#" data-bs-toggle="dropdown" style="margin-right: 20px;">
                         <div class="profile-info"> 
                             <span class="name">{{ auth()->user()->name }}</span>
-                            <span class="username">{{ '@' . auth()->user()->username }}</span>
                             <span class="role">{{ auth()->user()->roles->first() ? \Illuminate\Support\Str::headline(auth()->user()->roles->first()->name) : '' }}</span>
                         </div>
                         <i class="fa custom-caret"></i>
