543e75d5dcbfc5307b57da039c9616a5e3d124ab
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-18 21:12:56 +0500

    Update CostingController.php

--- app/Http/Controllers/CostingController.php
+++ app/Http/Controllers/CostingController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\{Vehicle, VehicleCosting};
+use App\Services\LedgerService;
 use Illuminate\Http\Request;
 
 class CostingController extends Controller
