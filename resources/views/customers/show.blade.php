9f1925d40a754626729bc924f90f78d54dcdd016
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-18 12:22:47 +0500

    Update show.blade.php

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -20,14 +20,16 @@
         <section class="card">
             <header class="card-header d-flex justify-content-between align-items-center">
                 <h2 class="card-title">{{ $customer->name }}</h2>
-                <a href="{{ route('customers.index') }}" class="btn btn-sm btn-default">
-                    <i class="fa fa-arrow-left"></i> Back to All Customers
-                </a>
-                @can('invoices.create')
-                    <a href="{{ route('invoices.bulk_create_form', $customer) }}" class="btn btn-sm btn-outline-primary mb-2">
-                        <i class="fa fa-file-invoice"></i> Bulk Generate Invoices
+                <div>
+                    <a href="{{ route('customers.index') }}" class="btn btn-sm btn-default">
+                        <i class="fa fa-arrow-left"></i> Back to All Customers
                     </a>
-                @endcan
+                    @can('invoices.create')
+                        <a href="{{ route('invoices.bulk_create_form', $customer) }}" class="btn btn-sm btn-outline-primary mb-2">
+                            <i class="fa fa-file-invoice"></i> Bulk Generate Invoices
+                        </a>
+                    @endcan
+                </div>
             </header>
 
             @include('customers._tabs', ['customer' => $customer, 'active' => 'overview'])
