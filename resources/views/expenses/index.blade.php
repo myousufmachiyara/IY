2b7e2477faef439baa3d1114e206374c0df422e8
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-19 22:06:08 +0500

    Fixes

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -166,8 +166,8 @@
                             </div>
                             <div class="col-lg-6 mb-2">
                                 <label>Date <span class="text-danger">*</span></label>
-                                <input type="date" class="form-control" name="expense_date" value="{{ date('Y-m-d') }}"
-                                    @unless(auth()->user()->isSuperAdmin()) min="{{ date('Y-m-d') }}" max="{{ date('Y-m-d') }}" readonly @endunless required>
+                                <input type="date" id="edit_exp_date" class="form-control" name="expense_date" value="{{ date('Y-m-d') }}"
+                                    @unless(auth()->user()->isSuperAdmin()) readonly @endunless required>
                             </div>
                             <div class="col-lg-6 mb-2">
                                 <label>Paid From <span class="text-danger">*</span></label>
