0eb2a415e14c64e648e56e0978d8e9155e93a060
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-20 23:17:45 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('customers/{customer}/invoices/merge', [InvoiceController::class, 'mergeSelectForm'])->middleware('permission:invoices.print')->name('invoices.merge_select');
     Route::post('customers/{customer}/invoices/merge', [InvoiceController::class, 'mergePdf'])->middleware('permission:invoices.print')->name('invoices.merge_pdf');
     Route::post('invoices/merge', [InvoiceController::class, 'mergeSelectedPdf'])->middleware('permission:invoices.print')->name('invoices.merge_selected');
-    
+    Route::get('vehicles/{vehicle}/deposit-invoice/pdf', [VehicleController::class, 'depositInvoicePdf'])->middleware('permission:vehicle_requirement.print')->name('vehicles.deposit_invoice_pdf');
     Route::get('payments', [PaymentController::class, 'index'])->middleware('permission:payments.index')->name('payments.index');
     Route::post('payments', [PaymentController::class, 'store'])->middleware('permission:payments.create')->name('payments.store');
     Route::post('payments/{payment}/approve', [PaymentController::class, 'approve'])->middleware('permission:payments.edit')->name('payments.approve');
