71e36aa1bce22263ef9e726fe73a9e19210fd295
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-20 19:51:19 +0500

    F

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 @endphp
 
 {{-- ===================== OVERVIEW ===================== --}}
-<h2 class="card title text-uppercase mt-2 mb-2">Overview</h2>
+<h3 class="card title text-uppercase mt-2 mb-2">Overview</h3>
 <div class="row">
     {!! statCard(auth()->user()->isSalesAgent() ? 'My Customers' : 'Total Customers', $stats['customers'], 'primary', route('customers.index'), false) !!}
     {!! statCard('Vehicles Bid', $stats['vehicles_bid'], 'tertiary', route('bids.index'), false) !!}
